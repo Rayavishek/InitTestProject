59fe5e175ea4700ce3f8c0709589d9b25b63ae68
rayavishek <rayavishek@gmail.com>
2016-10-25 09:18:45 +0530

    Kuchi testing

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -37,7 +37,7 @@ exports.handle = function handle(client) {
   },
 
   prompt() {
-    client.addTextResponse('Hi There! What\'s Up!')
+    client.addTextResponse('Hi Kuchi! Kuch bestest. Mostest. Foreverest! Hain...')
     client.done()
   }
 })
