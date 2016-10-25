a4df63100a78ba8a752dd5c9a775765283cb0de2
rayavishek <rayavishek@gmail.com>
2016-10-25 08:47:05 +0530

    First change to index,js to handle greeting & goodbye

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -31,14 +31,38 @@ exports.handle = function handle(client) {
     }
   })
 
+  const handleGreeting = client.createStep({
+  satisfied() {
+    return false
+  },
+
+  prompt() {
+    client.addTextResponse('Hello world, I mean human')
+    client.done()
+  }
+})
+
+  const handleGoodbye = client.createStep({
+  satisfied() {
+    return false
+  },
+
+  prompt() {
+    client.addTextResponse('Hope to see you back soon!')
+    client.done()
+  }
+})
+
   client.runFlow({
     classifications: {
 			// map inbound message classifications to names of streams
+			greeting: 'greeting'
+			goodbye: 'goodbye'
     },
-    autoResponses: {
-      // configure responses to be automatically sent as predicted by the machine learning model
-    },
-    streams: {
+   streams: {
+	     // Add a Stream for greetings and assign it a Step
+    greeting: handleGreeting,
+	goodbye: handleGoodbye,
       main: 'onboarding',
       onboarding: [sayHello],
       end: [untrained]
